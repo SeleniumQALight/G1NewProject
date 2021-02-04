83daba717e337797f93673b3a99da37a789450bc
Iryna_Lutskevych <ira98777@gmail.com>
2021-02-04 21:34:13 +0200

    Login test with page object

---
+++ lutskevych/src/test/java/loginTests/LoginTestWithPageObject.java
@@ -0,0 +1,17 @@
+package loginTests;
+
+import baseTest.BaseTest;
+import org.junit.Test;
+
+public class LoginTestWithPageObject extends BaseTest {
+    @Test
+    public void validLogin(){
+        loginPage.openLoginPage();
+        loginPage.enterLoginSignIn("auto");
+        loginPage.enterPasswordSignIn("123456qwerty");
+        loginPage.clickButtonSignIn();
+
+        checkExpectedResult("Button SignOut is not visible",homePage.isButtonSignOutVisible());
+    }
+
+}
