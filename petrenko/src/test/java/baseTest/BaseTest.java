52a456e1b923a807c35a18f3aa3bbc87c99e490f
Yura Petrenko <bald2003@ukr.net>
2021-02-05 16:05:06 +0200

    petrenko 05-02-21 unLoginTest

---
+++ petrenko/src/test/java/baseTest/BaseTest.java
@@ -0,0 +1,49 @@
+package baseTest;
+
+import org.apache.log4j.Logger;
+import org.junit.After;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import pages.HomePage;
+import pages.LoginPage;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+public class BaseTest {
+    public WebDriver webDriver;
+    Logger logger = Logger.getLogger(getClass());
+    protected LoginPage loginPage;
+    protected HomePage homePage;
+
+    @Before
+    public void setup() {
+        File fileFF = new File("./drivers/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", fileFF.getAbsolutePath());
+        webDriver = new ChromeDriver();
+
+        webDriver.manage().window().fullscreen();
+        webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        logger.info("Browser was open.");
+        loginPage = new LoginPage(webDriver);
+        homePage = new HomePage(webDriver);
+
+    }
+
+
+    @After
+    public void tearDown() {
+        webDriver.quit();
+        logger.info("Browser was closed");
+
+    }
+
+
+    protected void checkExpectedResult(String massage, boolean aktualResult){
+        Assert.assertTrue(massage, aktualResult);
+    }
+
+}
