c520c5090d07c9547b0d04131fa97d7bd91cc5cd
yaroslav kyrii <yaroslavkyrii1@gmail.com>
2021-02-15 13:45:35 +0200

    HomeWork3-DeletePost

--- kirii/src/main/java/pages/SinglePostPage.java
+++ kirii/src/main/java/pages/SinglePostPage.java
@@ -11,12 +11,19 @@ public class SinglePostPage extends ParentPage {
 
     @FindBy(xpath = ".//*[@class='alert alert-success text-center']")
     private WebElement successMessageElement;
+    @FindBy(xpath = ".//button[@class='delete-post-button text-danger']")
+    private WebElement deleteButton;
 
     public SinglePostPage(WebDriver webDriver) {
         super(webDriver);
     }
 
     public SinglePostPage checkIsRedirectToSinglePostPage(){
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         Assert.assertThat("Invalid page", webDriver.getCurrentUrl() , containsString("https://qa-complex-app-for-testing.herokuapp.com/post/")); //содержит ли частичный урл
                 //<!-- https://mvnrepository.com/artifact/org.hamcrest/hamcrest-all -->
                 //        <dependency>
@@ -33,4 +40,10 @@ public class SinglePostPage extends ParentPage {
         checkIsElementVisible(successMessageElement);
         return this;
     }
+
+    public ProfilePage clickOnDeleteButton(){
+        clickOnElement(deleteButton);
+        return new ProfilePage(webDriver);
+    }
+
 }
--- kirii/src/test/java/baseTest/BaseTest.java
+++ kirii/src/test/java/baseTest/BaseTest.java
@@ -6,6 +6,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import pages.CreatePostPage;
 import pages.HomePage;
 import pages.LoginPage;
 
@@ -17,6 +18,7 @@ public class BaseTest {
     protected Logger logger = Logger.getLogger(getClass());
     protected LoginPage loginPage;
     protected HomePage homePage;
+    protected CreatePostPage createPostPage;
 
     @Before
     public void setUp() {
@@ -29,6 +31,7 @@ public class BaseTest {
         logger.info("Browser was opened");
         loginPage = new LoginPage(webDriver);
         homePage = new HomePage(webDriver);
+        createPostPage = new CreatePostPage(webDriver);
     }
     @After
     public void tearDown(){
