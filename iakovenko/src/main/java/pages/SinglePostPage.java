d7d57fa47784778283a070a8615c587532d2e0d0
Lenovo <yakovenkors@gmail.com>
2021-02-16 21:20:20 +0200

    Lesson on 11-02-2021

--- iakovenko/src/main/java/pages/SinglePostPage.java
+++ iakovenko/src/main/java/pages/SinglePostPage.java
@@ -1,5 +1,6 @@
 package pages;
 
+import libs.Util;
 import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -10,11 +11,14 @@ import static org.hamcrest.Matchers.containsString;
 public class SinglePostPage extends ParentPage {
     @FindBy (xpath = ".//*[@class='alert alert-success text-center']")
     private WebElement successMessageElement;
+    @FindBy (xpath = ".//button[@class='delete-post-button text-danger']")
+    private WebElement deleteButton;
 
     public SinglePostPage(WebDriver webDriver) {
         super(webDriver);
     }
     public SinglePostPage checkIsRedirectToSinglePostPage(){
+        Util.waitABit(2);
         Assert.assertThat("Invalid page",webDriver.getCurrentUrl(),
                 containsString("https://qa-complex-app-for-testing.herokuapp.com/post/") );
         return this;
@@ -22,5 +26,10 @@ public class SinglePostPage extends ParentPage {
     public SinglePostPage checkIsSuccessMessageDisplayed(){
         checkIsElementVisible(successMessageElement);
         return this;
+    }
+
+    public ProfilePage clickOnDeleteButton() {
+        clickOnElement(deleteButton);
+        return new ProfilePage(webDriver);
     }
 }
--- iakovenko/src/test/java/posts/CreateNewPostTest.java
+++ iakovenko/src/test/java/posts/CreateNewPostTest.java
@@ -6,21 +6,30 @@ import org.junit.Test;
 import pages.LoginPage;
 
 public class CreateNewPostTest extends BaseTest {
+    final String POST_TITLE = "Roman Title of Post";
     @Test
     public void createNewPost(){
     loginPage.loginWithValidCred()
              .checkIsButtonSignOutVisible()
             .clickOnCreatePostButton()
             .checkIsRedirectedOnCreatePostPage()
-            .enterTitleInToInputTitle(" Roman Title of Post")
+            .enterTitleInToInputTitle(POST_TITLE)
             .enterTextInToInputBody("Post body")
             .clickOnButtonSaveNewPost()
             .checkIsRedirectToSinglePostPage()
             .checkIsSuccessMessageDisplayed()
+
     ;
     }
     @After
     public void deletePost(){
+        homePage
+                .openHomePage()
+                .checkIsButtonSignOutVisible()
+                .clickOnProfileButton()
+        .checkIsRedirectToProfilePage()
+            .deletePostWhilePresent(POST_TITLE);
+        ;
 
     }
 
