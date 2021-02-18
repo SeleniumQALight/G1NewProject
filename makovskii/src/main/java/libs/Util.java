7f61476eaa8ff90146ed05b91dce2a8a30db0974
MakovskyiNikolai <makovskyinikolai@gmail.com>
2021-02-18 08:44:00 +0200

    Create Post Test After

---
+++ makovskii/src/main/java/libs/Util.java
@@ -0,0 +1,16 @@
+package libs;
+
+public class Util {
+
+    public static void waitABit(Integer second){
+        try {
+            Thread.sleep(second*1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+}
