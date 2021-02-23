5dd938eef4c2a1cbea1b468e6f9a667fe3e1439f
Taras Radulenko <Taras_Radulenko@epam.com>
2021-02-23 21:15:09 +0200

    HtmlElements

--- overAll/src/main/java/pages/ParentPage.java
+++ overAll/src/main/java/pages/ParentPage.java
@@ -10,16 +10,28 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class ParentPage {
+import ru.yandex.qatools.htmlelements.element.TypifiedElement;
+import ru.yandex.qatools.htmlelements.loader.decorator.HtmlElementDecorator;
+import ru.yandex.qatools.htmlelements.loader.decorator.HtmlElementLocatorFactory;
+
+abstract class ParentPage {
     protected WebDriver webDriver;
     protected WebDriverWait webDriverWait10, webDriverWait15;
+    protected final String baseUrl = "https://qa-complex-app-for-testing.herokuapp.com";
     Logger logger = Logger.getLogger(getClass());
     public ParentPage (WebDriver webDriver){
         this.webDriver = webDriver;
-        PageFactory.initElements(webDriver, this);
+//        PageFactory.initElements(webDriver, this);
+        PageFactory.initElements(
+                new HtmlElementDecorator(
+                        new HtmlElementLocatorFactory(webDriver))
+                ,this);
+
         webDriverWait10 = new WebDriverWait(webDriver, 10);
         webDriverWait15 = new WebDriverWait(webDriver, 15);
     }
+
+    abstract String getRelativeUrl();
 
     protected void waitChatToBeHide(){
         webDriverWait10.until(ExpectedConditions
@@ -31,17 +43,34 @@ public class ParentPage {
             webDriverWait15.until(ExpectedConditions.visibilityOf(webElement));
             webElement.clear();
             webElement.sendKeys(text);
-            logger.info(text + " was inputted into element");
+            logger.info(text + " was inputted into element" + getElementName(webElement));
         }catch (Exception e){
             printErrorMessageAndStopTest(e);
         }
+    }
+
+    private String getElementName(WebElement webElement) {
+        String elementName = "";
+        if (webElement instanceof TypifiedElement){
+            elementName = " '" + ((TypifiedElement) webElement).getName() + "' ";
+        }
+        return elementName;
     }
 
     protected void clickOnElement(WebElement webElement){
         try{
             webDriverWait15.until(ExpectedConditions.elementToBeClickable(webElement));
             webElement.click();
-            logger.info("Element was clicked");
+            logger.info(getElementName(webElement) + "Element was clicked");
+        }catch (Exception e){
+            printErrorMessageAndStopTest(e);
+        }
+    }
+    protected void clickOnElement(WebElement webElement , String elementName){
+        try{
+            webDriverWait15.until(ExpectedConditions.elementToBeClickable(webElement));
+            webElement.click();
+            logger.info(elementName + "Element was clicked");
         }catch (Exception e){
             printErrorMessageAndStopTest(e);
         }
@@ -50,10 +79,10 @@ public class ParentPage {
     protected boolean isElementDisplayed(WebElement webElement){
         try {
             boolean state = webElement.isDisplayed();
-            logger.info("Element displayed : " + state);
+            logger.info(getElementName(webElement) + "Element displayed : " + state);
             return state;
         }catch (Exception e){
-            logger.info("Element displayed : false");
+            logger.info(getElementName(webElement) + "Element displayed : false");
             return false;
         }
     }
@@ -67,7 +96,7 @@ public class ParentPage {
         try{
             Select select = new Select(webElement);
             select.selectByVisibleText(text);
-            logger.info(text + " was selected in DropDown");
+            logger.info(text + " was selected in DropDown" + getElementName(webElement));
         }catch (Exception e){
             printErrorMessageAndStopTest(e);
         }
@@ -77,7 +106,7 @@ public class ParentPage {
         try{
             Select select = new Select(webElement);
             select.selectByValue(value);
-            logger.info(value + " was selected in DropDown");
+            logger.info(value + " was selected in DropDown" + getElementName(webElement));
         }catch (Exception e){
             printErrorMessageAndStopTest(e);
         }
--- overAll/src/main/java/pages/ProfilePage.java
+++ overAll/src/main/java/pages/ProfilePage.java
@@ -11,22 +11,28 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 import libs.Util;
+import ru.yandex.qatools.htmlelements.element.TextInput;
 
 public class ProfilePage extends ParentPage {
 
     final String postTitleLocator = ".//*[text()='%s']";
 
     @FindBy(xpath = ".//*[contains(text(), 'successfully deleted')]")
-    private WebElement successPostDeleteElement;
+    private TextInput successPostDeleteElement;
 
     public ProfilePage(WebDriver webDriver) {
         super(webDriver);
     }
 
+    @Override
+    String getRelativeUrl() {
+        return "/profile";
+    }
+
     public ProfilePage checkIsRedirectToProfilePage(){
         waitChatToBeHide();
         Assert.assertThat(webDriver.getCurrentUrl()
-                , containsString("https://qa-complex-app-for-testing.herokuapp.com/profile"));
+                , containsString(baseUrl + getRelativeUrl()));
         return this;
     }
 
@@ -37,7 +43,7 @@ public class ProfilePage extends ParentPage {
         int counter = 0;
         while (!listOfPosts.isEmpty() && counter < 100){
             clickOnElement(webDriver.findElement(
-                    By.xpath(String.format(postTitleLocator, post_title))));
+                    By.xpath(String.format(postTitleLocator, post_title))), " Post with title " + post_title  );
             new SinglePostPage(webDriver)
                     .checkIsRedirectToSinglePostPage()
                     .clickOnDeleteButton()
