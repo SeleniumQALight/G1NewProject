9436b38ecafc0f30d58a797bf0e805cded3f7e6a
Vera <veralungol@gmail.com>
2021-02-23 19:42:58 +0200

    checkIsPostAdded

--- lungol/src/main/java/pages/ProfilePage.java
+++ lungol/src/main/java/pages/ProfilePage.java
@@ -53,4 +53,10 @@ public class ProfilePage extends ParentPage {
         checkIsElementVisible(successPostDeleteElement);
         return this;
     }
+
+    public ProfilePage checkIsPostWasAdded(String post_title) {
+        List<WebElement> postsList = webDriver.findElements(By.xpath(String.format(postTitleLocator, post_title)));
+        Assert.assertEquals("Number of posts with title " + post_title,1, postsList.size());
+        return this;
+    }
 }
