5346b6080defb8c8f23c5c6695372fc8d9c57f61
Taras Radulenko <Taras_Radulenko@epam.com>
2021-02-23 15:49:01 +0200

    broken invalidtest

--- overAll/src/test/java/loginTests/LoginTestWithPageObject.java
+++ overAll/src/test/java/loginTests/LoginTestWithPageObject.java
@@ -8,7 +8,7 @@ public class LoginTestWithPageObject extends BaseTest {
     @Test
     public void validLogin(){
         loginPage.openLoginPage();
-        loginPage.enterLoginSignIn("auto1");
+        loginPage.enterLoginSignIn("auto");
         loginPage.enterPassWordSignIn("123456qwerty");
         loginPage.clickButtonSignIn();
 
@@ -18,7 +18,7 @@ public class LoginTestWithPageObject extends BaseTest {
 
     @Test
     public void inValidLogin(){
-        loginPage.fillLoginFormAndSubmit("WrongLogin", "123456qwerty");
+        loginPage.fillLoginFormAndSubmit("auto", "123456qwerty");
 
         checkExpectedResult("Button SignOut is visible, but should `t"
                 , !homePage.isButtonSignOutVisible());
