1a5e6f3e6b11c73fc1bcb9038fb9e8932ccd2805
Taras Radulenko <Taras_Radulenko@epam.com>
2021-02-23 16:08:52 +0200

    SmokeSuite

---
+++ overAll/src/test/java/suits/SmokeSuit.java
@@ -0,0 +1,17 @@
+package suits;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import loginTests.LoginTestWithPageObject;
+import posts.CreateNewPostTest;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses(
+        {
+                LoginTestWithPageObject.class,
+                CreateNewPostTest.class
+        }
+)
+public class SmokeSuit {
+}
