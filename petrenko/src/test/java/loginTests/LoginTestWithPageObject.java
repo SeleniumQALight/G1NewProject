23665b3be4c694a66003c58739c265200374dd19
Yura Petrenko <bald2003@ukr.net>
2021-02-25 21:21:50 +0200

    petrenko RunThis

--- petrenko/src/test/java/loginTests/LoginTestWithPageObject.java
+++ petrenko/src/test/java/loginTests/LoginTestWithPageObject.java
@@ -1,12 +1,19 @@
 package loginTests;
 
 import baseTest.BaseTest;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import junitparams.naming.TestCaseName;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitParamsRunner.class)
 
 public class LoginTestWithPageObject extends BaseTest {
 
+
     @Test
-    public void validLogin(){
+    public void validLogin() {
 
         loginPage.fillLoginFormAndSubmit("auto", "123456qwerty");
         checkExpectedResult("Button Sign In is not visible.", homePage.isButtonSignOutVisible());
@@ -14,11 +21,18 @@ public class LoginTestWithPageObject extends BaseTest {
     }
 
     @Test
-    public void unValidLogin(){
+    @Parameters({
+     "qwerty, 123456qwerty",
+            ", 123456qwerty"
+
+
+    })
+    @TestCaseName("inValidLogin: login = {0}, passWord = {1}")
+    public void unValidLogin(String login, String password) {
 
         loginPage.openLoinPage();
-        loginPage.enterLoginSignIn("qwerty");
-        loginPage.enterPassWordSignIn("123456qwerty");
+        loginPage.enterLoginSignIn(login);
+        loginPage.enterPassWordSignIn(password);
         loginPage.clickButtonSignIn();
         checkExpectedResult("Button Sign In is visible.", homePage.isButtonSignOutNotVisible());
 
---
+++ petrenko/src/test/java/suits/Smoke.java
@@ -0,0 +1,21 @@
+package suits;
+
+import loginTests.LoginTestWithPageObject;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import pages.CreatePostPage;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses(
+        {
+                LoginTestWithPageObject.class,
+                CreatePostPage.class
+
+
+
+
+        }
+)
+public class Smoke {
+
+}
