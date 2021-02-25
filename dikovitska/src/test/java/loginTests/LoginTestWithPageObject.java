8e525c308de1d3088206d852136a946614391d85
yuliia.dykovytska <yuliya.dikovitskaya@gmail.com>
2021-02-25 21:31:12 +0200

    CrossBrowser testing + correct log

--- dikovitska/src/test/java/loginTests/LoginTestWithPageObject.java
+++ dikovitska/src/test/java/loginTests/LoginTestWithPageObject.java
@@ -1,9 +1,14 @@
 package loginTests;
 
 import baseTest.BaseTest;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import junitparams.naming.TestCaseName;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import pages.LoginPage;
 
+@RunWith(JUnitParamsRunner.class)
 public class LoginTestWithPageObject extends BaseTest {
 @Test
     public void ValidLogin() {
@@ -17,13 +22,17 @@ public class LoginTestWithPageObject extends BaseTest {
 
 
     @Test
-    public void inValidLogin(){
-        loginPage.openLoginPage();
-        loginPage.enterLoginSignIn("wrong Login");
-        loginPage.enterPassWordSignIn("123456qwerty");
-        loginPage.clickButtonSignIn();
+    @Parameters({
+            "WrongLogin,123456qwerty",
+            ",123456qwerty"
+    })
+    @TestCaseName("inValidLogin: login = {1}, password = {2}")
 
-        checkExpectedResult("Button SignOut is visible, but button should not be visible", !homePage.isButtonSignOutVisible());
+    public void inValidLogin(String login, String pass){
+        loginPage.fillLoginFormAndSubmit(login, pass);
+
+        checkExpectedResult("Button SignOut is visible, but should `t"
+                , !homePage.isButtonSignOutVisible());
 
     }
 
