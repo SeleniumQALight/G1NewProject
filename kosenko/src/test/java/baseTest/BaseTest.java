015dfb350be418c2f75adec57021a9817589ce35
Anastasia Kosenko <people.1198@gmail.com>
2021-02-25 21:23:18 +0200

    Suits, browsers, parametrised tests

--- kosenko/src/test/java/baseTest/BaseTest.java
+++ kosenko/src/test/java/baseTest/BaseTest.java
@@ -1,12 +1,17 @@
 package baseTest;
 
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.log4j.Logger;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.TestName;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import pages.HomePage;
 import pages.LoginPage;
 import pages.SinglePostPage;
@@ -21,14 +26,17 @@ public class BaseTest {
         protected Logger logger = Logger.getLogger(getClass());
         protected LoginPage loginPage;
         protected HomePage homePage;
-        protected SinglePostPage singlePostPage;
+        //protected SinglePostPage singlePostPage;
 
+        @Rule
+        public TestName testName = new TestName();
 
         @Before
-        public void setUp(){
-            File fileFF = new File("./drivers/chrome-driver");
-            System.setProperty("webdriver.chrome.driver", fileFF.getAbsolutePath());
-            webDriver = new ChromeDriver();
+        public void setUp() throws Exception {
+            logger.info("----------" + testName.getMethodName() + " was started ------");
+            //File fileFF = new File("./drivers/chrome-driver");
+            //System.setProperty("webdriver.chrome.driver", fileFF.getAbsolutePath());
+            webDriver = initDriver();
 
             webDriver.manage().window().maximize();  //full size for window
             webDriver.manage().timeouts().implicitlyWait(configProperties.TIME_FOR_DFFAULT_WAIT(), TimeUnit.SECONDS); //default time for waiting
@@ -36,10 +44,28 @@ public class BaseTest {
             loginPage = new LoginPage(webDriver); // from this go to constructor
             homePage = new HomePage(webDriver);
         }
-        @After
-        public void teamDown(){
+
+    private WebDriver initDriver() throws Exception {
+            String browser = System.getProperty("browser");
+            if ((browser == null) || ("chrome".equalsIgnoreCase(browser)) ){
+                WebDriverManager.chromedriver().setup();
+                return new ChromeDriver();
+            } else if ("firefox".equalsIgnoreCase(browser)) {
+                WebDriverManager.firefoxdriver().setup();
+                return new FirefoxDriver();
+            } else if ("ie".equalsIgnoreCase(browser)) {
+                WebDriverManager.iedriver().setup();
+                return new InternetExplorerDriver();
+            } else {
+                throw new Exception("Check browser var");
+            }
+    }
+
+    @After
+        public void tearDown(){
             webDriver.quit();
             logger.info("Browser was closed");
+        logger.info("----------" + testName.getMethodName() + " was ended ------");
 
         }
 
---
+++ kosenko/src/test/java/suits/Smoke.java
@@ -0,0 +1,17 @@
+package suits;
+
+import loginTests.LoginTestWithPageObject;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import posts.CreateNewPostTest;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses(
+        {
+                LoginTestWithPageObject.class,
+                CreateNewPostTest.class,
+        }
+)
+
+public class Smoke {
+}
