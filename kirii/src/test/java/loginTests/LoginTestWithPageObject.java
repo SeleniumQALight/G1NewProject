4a10607cb9416eba33819a3d9302beaca0200027
yaroslav kyrii <yaroslavkyrii1@gmail.com>
2021-02-25 21:23:14 +0200

    Suits, browsers, parametrized tests

--- kirii/src/test/java/loginTests/LoginTestWithPageObject.java
+++ kirii/src/test/java/loginTests/LoginTestWithPageObject.java
@@ -1,9 +1,16 @@
 package loginTests;
 
 import baseTest.BaseTest;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import junitparams.naming.TestCaseName;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(JUnitParamsRunner.class)
 public class LoginTestWithPageObject extends BaseTest {
+
+
     @Test
     public void validLogIn(){
         loginPage.openLoginPage();
@@ -12,5 +19,17 @@ public class LoginTestWithPageObject extends BaseTest {
         loginPage.clickButtonSignIn();
 
         checkExpectedResult("Button SignOut is not visible", homePage.isButtonSignOutVisible());
+    }
+
+    @Test
+    @Parameters({
+            "WrongLogin,123456qwerty",
+            ",123456qwerty"
+    })
+    @TestCaseName("inValidLogin: login = {0}, password = {1}")
+    public void invalidLogIn(String login, String pass){
+        loginPage.fillLoginFormAndSubmit(login, pass);
+
+        checkExpectedResult("Button SignOut is visible, but shouldn't 't", !homePage.isButtonSignOutVisible());
     }
 }
