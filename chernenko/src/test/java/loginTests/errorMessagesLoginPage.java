57ea74d92aea0f1aef85946e934b1d648992f09f
Lena <lenazurich@gmail.com>
2021-02-28 22:08:00 +0100

    Test with parameters checkErrorMessages on the LoginPage was added.

--- chernenko/src/test/java/loginTests/errorMessagesLoginPage.java
+++ chernenko/src/test/java/loginTests/errorMessagesLoginPage.java
@@ -1,11 +1,15 @@
 package loginTests;
 
 import baseTest.BaseTest;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import junitparams.naming.TestCaseName;
 import libs.TestData;
 import libs.Util;
 import org.junit.Assert;
 import org.junit.Test;
 
+import org.junit.runner.RunWith;
 import org.openqa.selenium.WebElement;
 
 
@@ -13,7 +17,7 @@ import org.openqa.selenium.WebElement;
 import java.lang.ref.SoftReference;
 import java.util.ArrayList;
 import java.util.List;
-
+@RunWith(JUnitParamsRunner.class)
 public class errorMessagesLoginPage extends BaseTest {
 
     @Test
@@ -21,7 +25,7 @@ public class errorMessagesLoginPage extends BaseTest {
         // Use for the test date from TestData file
         loginPage.fillLoginFormNewUserAndSubmit(TestData.VALID_UNIQUE_USERNAME,TestData.INVALID_EMAIL,TestData.VALID_PASSWORD);
         List<WebElement> list = new ArrayList<>();
-        list = loginPage.numberOfErrorMessageWereDisplayed();
+        list = loginPage.listOfErrorMessageWereDisplayed();
         Assert.assertEquals("Wrong numbers of messages ",1,list.size());
     }
 
@@ -31,7 +35,7 @@ public class errorMessagesLoginPage extends BaseTest {
         // Use for the test date from TestData file
         loginPage.fillLoginFormNewUserAndSubmit(TestData.INVALID_SHORT_USERNAME,TestData.VALID_UNIQUE_USER_EMAIL,TestData.INVALID_SHORT_PASSWORD);
         List<WebElement> list = new ArrayList<>();
-        list = loginPage.numberOfErrorMessageWereDisplayed();
+        list = loginPage.listOfErrorMessageWereDisplayed();
         Assert.assertEquals("Wrong numbers of messages ",2,list.size());
     }
 
@@ -40,7 +44,21 @@ public class errorMessagesLoginPage extends BaseTest {
         // Use for the test date from TestData file
         loginPage.fillLoginFormNewUserAndSubmit(TestData.INVALID_LONG_USERNAME,TestData.INVALID_EMAIL,TestData.INVALID_LONG_PASSWORD);
         List<WebElement> list = new ArrayList<>();
-        list = loginPage.numberOfErrorMessageWereDisplayed();
+        list = loginPage.listOfErrorMessageWereDisplayed();
         Assert.assertEquals("Wrong numbers of messages ",3,list.size());
+    }
+
+    @Test
+    @Parameters({
+            "qa,test_unique@test.com,123456qwerty, 1",
+            "testnameunique,test_unique,123, 2",
+            "1234567890qwertyuiopqwertyuiop1,test,123456qwerty123456qwerty123456qwerty123456qwerty123456qwerty, 3"
+    })
+    @TestCaseName(
+            "numbersOfMessagesWereDisplayed: username = {1}, email = {2}, password = {3} "
+    )
+    public void checkErrorMessagesTestWithParameters(String username, String email, String password, int expectedResult){
+        loginPage.fillLoginFormNewUserAndSubmit(username, email, password);
+        Assert.assertEquals("ERROR: The number of displayed erros is wrong. ", expectedResult, loginPage.numberOfErrorMessageWereDisplayed());
     }
 }
