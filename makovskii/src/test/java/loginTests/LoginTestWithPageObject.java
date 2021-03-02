b028993e07ed0f1f822f89db61b1870757618c73
MakovskyiNikolai <makovskyinikolai@gmail.com>
2021-03-02 21:28:46 +0200

    Excel

--- makovskii/src/test/java/loginTests/LoginTestWithPageObject.java
+++ makovskii/src/test/java/loginTests/LoginTestWithPageObject.java
@@ -1,36 +1,42 @@
 package loginTests;
 import baseTest.BaseTest;
-import junitparams.JUnitParamsRunner;
-import junitparams.Parameters;
-import junitparams.naming.TestCaseName;
+import libs.SpreadsheetData;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import pages.ParentPage;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collection;
 
 
-@RunWith(JUnitParamsRunner.class)
+@RunWith(Parameterized.class)
 public class LoginTestWithPageObject extends BaseTest {
-    @Test
-    public void validLogin(){
-        loginPage.openLoginPage();
-        loginPage.enterLoginSignIn("auto");
-        loginPage.enterPassWordSignIn("123456qwerty");
-        loginPage.clickButtonSignIn();
+    private String login, pass;
 
-        checkExpectedResult("Button SignOut is not visible",
-                homePage.isButtonSignOutVisible());
+    public LoginTestWithPageObject(String login, String pass) {
+        this.login = login;
+        this.pass = pass;
     }
 
+
+
+    @Parameterized.Parameters
+    public static Collection testData() throws IOException {
+        InputStream speadsSheet = new FileInputStream(ParentPage.configProperties.DATA_FILE_PATH() + "testDataSuit.xls");
+        return new SpreadsheetData(speadsSheet, "InvalidLogOn").getData();
+    }
+
+
     @Test
-    @Parameters({
-            "WrongLogin,123456qwerty",
-            ",123456qwerty"
-    })
-    @TestCaseName("inValidLogin: login = {0}, password = {1}")
-    public void inValidLogin(String login, String pass){
+    public void inValidLogin(){
         loginPage.filLoginFormAndSubmit(login,pass);
 
-        checkExpectedResult("Button SignOut is visible, but should not",
-                !homePage.isButtonSignOutVisible());
+        checkExpectedResult("Button SignOut is visible, but should not"
+                , !homePage.isButtonSignOutVisible());
     }
 
 
