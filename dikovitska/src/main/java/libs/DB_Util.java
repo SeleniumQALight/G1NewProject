9e36ac3356191843846e66a67ade3877cd232f4a
yuliia.dykovytska <yuliya.dikovitskaya@gmail.com>
2021-03-04 21:11:50 +0200

    Tests with DataBase

---
+++ dikovitska/src/main/java/libs/DB_Util.java
@@ -0,0 +1,23 @@
+package libs;
+
+import org.apache.log4j.Logger;
+
+import java.sql.SQLException;
+
+public class DB_Util {
+    private Database mySQL_Database;
+    Logger logger = Logger.getLogger(getClass());
+
+    public String getPassForLogin(String login) throws SQLException, ClassNotFoundException {
+        mySQL_Database = MySQL_Database.getDataBase(); //установили коннекшн
+        logger.info("----Connected to DB-----");
+
+        String pass = mySQL_Database.selectValue(
+                String.format("select passWord from seleniumTable where login = '%s'", login)
+        );
+        mySQL_Database.quit(); //разрываем коннекшн
+        return pass;
+
+    }
+
+}
