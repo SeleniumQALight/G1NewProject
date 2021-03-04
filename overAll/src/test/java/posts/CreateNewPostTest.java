223caba918517ecffe7e6904a8f3fea60de1b2a1
Taras Radulenko <Taras_Radulenko@epam.com>
2021-03-04 21:25:34 +0200

    Categories

--- overAll/src/test/java/posts/CreateNewPostTest.java
+++ overAll/src/test/java/posts/CreateNewPostTest.java
@@ -2,13 +2,15 @@ package posts;
 
 import org.junit.After;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
 import baseTest.BaseTest;
+import categories.SmokeTests;
 import libs.Util;
 
 public class CreateNewPostTest extends BaseTest {
     final String POST_TITLE = "Taras Title of Post" + Util.getDateAndTimeFormated();
-
+    @Category(SmokeTests.class)
     @Test
     public void createNewPost(){
         loginPage.loginWithValidCred()
