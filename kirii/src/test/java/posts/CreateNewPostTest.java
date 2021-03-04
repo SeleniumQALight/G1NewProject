25cec007fc0bd33ddf4f101728e4f8ba5f82e5dd
yaroslav kyrii <yaroslavkyrii1@gmail.com>
2021-03-04 21:25:33 +0200

    Categories

--- kirii/src/test/java/posts/CreateNewPostTest.java
+++ kirii/src/test/java/posts/CreateNewPostTest.java
@@ -1,13 +1,15 @@
 package posts;
 
 import baseTest.BaseTest;
+import categories.SmokeTests;
 import libs.Util;
 import org.junit.After;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
 public class CreateNewPostTest extends BaseTest {
     final String POST_TITLE = "Yaroslav6 Title of Post" + Util.getDateAndTimeFormated();
-
+    @Category(SmokeTests.class)
     @Test
     public void createNewPost(){
         loginPage.loginWithValidCred()
