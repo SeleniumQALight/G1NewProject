74b79aeb429e7d5066ff3f0c06e17d828bc34bd1
Yura Petrenko <bald2003@ukr.net>
2021-03-10 06:57:05 +0200

    petrenko  report allure began to work

--- petrenko/src/test/java/regestrationTests/CheckErrorMessage.java
+++ petrenko/src/test/java/regestrationTests/CheckErrorMessage.java
@@ -17,9 +17,9 @@ public class CheckErrorMessage extends BaseTest {
     @Test
     @Parameters({
            "qwerty,bald2004@ukr.nrtet,123456qwerty, 0, ",
-            ",abc@,123456qwerty, 2,Username must be at least 3 characters.;You must provide a valid email address.",
+            ",abc@,123456qwerty, 2,Username must be at least 3333 characters.;You must provide a valid email address.",
             ",abc@,56qwerty, 3,Username must be at least 3 characters.;You must provide a valid email address.;Password must be at least 12 characters.",
-            "qwerty,bald2004@ukr.nrtet,12,1,Password must be at least 12 characters."
+            "qwerty,bald2004@ukr.nrtet,12,1,Password must be at least 121221 characters."
     })
     @TestCaseName("checkErrorMessageInRegisterForm: userName = {0}, email = {1}, password = {2}, countUnValidValue = {3}, textOfErrorMessages = {4}")
 
