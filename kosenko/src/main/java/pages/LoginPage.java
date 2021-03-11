633b5b2949da6379042212b015716a696cc4c15c
Anastasia Kosenko <people.1198@gmail.com>
2021-03-12 01:46:13 +0200

    LoginPage+ValidationPage

--- kosenko/src/main/java/pages/LoginPage.java
+++ kosenko/src/main/java/pages/LoginPage.java
@@ -2,12 +2,19 @@ package pages;
 
 import io.qameta.allure.Step;
 import libs.TestData;
+import org.assertj.core.api.SoftAssertionError;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import ru.yandex.qatools.htmlelements.element.Button;
 import ru.yandex.qatools.htmlelements.element.TextInput;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LoginPage extends ParentPage {
     @FindBy(xpath = ".//*[@placeholder='Username']")
@@ -19,6 +26,13 @@ public class LoginPage extends ParentPage {
 
     @FindBy(xpath = ".//button[text()='Sign In']")
     private Button buttonSignIn;
+
+    @FindBy (xpath = ".//*[@id = 'email-register']")
+    private TextInput emailForRegistration;
+    @FindBy (xpath = ".//*[@id ='password-register']")
+    private WebElement passwordForRegistration;
+    @FindBy (xpath = ".//*[@id = 'username-register']")
+    private TextInput userNameInput;
 
 
     public LoginPage(WebDriver webDriver) {
@@ -62,6 +76,41 @@ public class LoginPage extends ParentPage {
         public HomePage loginWithValidCred(){
         fillLoginFormAndSubmit(TestData.VALID_LOGIN, TestData.VALID_PASSWORD);
         return new HomePage(webDriver);
+        }
+        @Step
+    public void enterUserName(String userName) {
+        enterTextInToElement(userNameInput, userName);
+        }
+        @Step
+    public void enterEmail(String email) {
+        enterTextInToElement(emailForRegistration, email);
+        }
+        @Step
+    public void enterPassWord(String passWord) {
+        enterTextInToElement(passwordForRegistration, passWord);
+        }
+        @Step
+    public void checkErrors(String errors) {
+        String[] errorsArray = errors.split(";");
+
+            List<WebElement> actualErrorsList = webDriver.findElements(By.xpath(
+                    ".//*[@class='alert alert-danger small liveValidateMessage liveValidateMessage--visible']"));
+
+            Assert.assertEquals("Number of Messages", errorsArray.length, actualErrorsList.size());
+
+            SoftAssertions softAssertions = new SoftAssertions();
+
+            ArrayList<String> textFromErrors = new ArrayList<>();
+
+            for (WebElement element : actualErrorsList){
+                textFromErrors.add(element.getText());
+
+            }
+            for (int i = 0; i < errorsArray.length; i++) {
+                softAssertions.assertThat(errorsArray[i]).isIn(textFromErrors);
+
+            }
+            softAssertions.assertAll();
         }
     }
 
