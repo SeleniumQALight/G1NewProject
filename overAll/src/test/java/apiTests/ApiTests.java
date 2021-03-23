025149035c2e18cf269f67b8f7a9b627ac61fb93
Taras Radulenko <Taras_Radulenko@epam.com>
2021-03-23 15:18:16 +0200

    apiTest response asString, jsonPath

--- overAll/src/test/java/apiTests/ApiTests.java
+++ overAll/src/test/java/apiTests/ApiTests.java
@@ -3,13 +3,20 @@ package apiTests;
 import static api.EndPoints.POST_BY_USER;
 import static io.restassured.RestAssured.given;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.assertj.core.api.MapAssert;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.Assert;
 import org.junit.Test;
 
 import api.AutorDTO;
 import api.PostDTO;
+import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 
 public class ApiTests {
     @Test
@@ -48,4 +55,44 @@ public class ApiTests {
 
 
     }
+
+    @Test
+    public void getAllPostsByUserNegativ() {
+
+        String userName = "autoapi";
+
+        String responseBody = given()
+                .contentType(ContentType.JSON).log().all()
+                .when()
+                .get(POST_BY_USER, userName+3333)
+                .then()
+                .statusCode(200).log().all()
+                .extract().response().getBody().asString();
+        Assert.assertEquals("","\"Sorry, invalid user requested.undefined\"", responseBody);
+    }
+
+    @Test
+    public void getAllPostsByUserValidatePath() {
+
+        String userName = "autoapi";
+
+        Response response=  given()
+                .contentType(ContentType.JSON).log().all()
+                .when()
+                .get(POST_BY_USER, userName)
+                .then()
+                .statusCode(200).log().all()
+                .extract().response();
+        List<String> titleList = response.jsonPath().getList("title", String.class);
+        List<Map> authorList = response.jsonPath().getList("author", Map.class);
+
+        SoftAssertions softAssertions = new SoftAssertions();
+        for (int i = 0; i < titleList.size(); i++) {
+            softAssertions.assertThat(titleList.get(i)).contains("test").as("Item number " + i);
+            softAssertions.assertThat(authorList.get(i).get("username")).as("Item number " + i).isEqualTo(userName+1);
+        }
+        softAssertions.assertAll();
+    }
+
+
 }
