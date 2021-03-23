a84fcce76343026184c155170672ceb43db51456
Julia <jkosolap@gmail.com>
2021-03-23 21:34:10 +0200

    ApiTest Response Validation

--- kosolap/src/main/java/api/EndPoints.java
+++ kosolap/src/main/java/api/EndPoints.java
@@ -1,6 +1,7 @@
 package api;
 
-public interface EndPoints {
+public interface
+EndPoints {
     String baseUrl = "http://qa-complex-app-for-testing.herokuapp.com";
     String POST_BY_USER = baseUrl + "/api/postsByAuthor/{1}";
 }
