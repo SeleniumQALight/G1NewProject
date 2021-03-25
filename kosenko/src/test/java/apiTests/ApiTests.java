834faff0ae08d08457597e9fc126d62ead3e6892
Anastasia Kosenko <people.1198@gmail.com>
2021-03-25 17:30:38 +0200

    apiTest response validation

--- kosenko/src/test/java/apiTests/ApiTests.java
+++ kosenko/src/test/java/apiTests/ApiTests.java
@@ -1,20 +1,98 @@
 package apiTests;
 
-import api.EndPoints;
-import io.restassured.http.ContentType;
-import org.junit.Test;
-
 import static api.EndPoints.POST_BY_USER;
 import static io.restassured.RestAssured.given;
 
+import java.util.List;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.Assert;
+import org.junit.Test;
+
+import api.AuthorDTO;
+import api.EndPoints;
+import api.PostDTO;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
 public class ApiTests {
+    final String USER_NAME = "autoapi";
+    Logger logger = Logger.getLogger(getClass());
+
     @Test
     public void getAllPostsByUser(){
-        given()
+        PostDTO[] responseBody = given()
                 .contentType(ContentType.JSON).log().all()
                 .when()
-                .get(POST_BY_USER, "auto")
+                .get(POST_BY_USER, USER_NAME)
                 .then()
-                .statusCode(200).log().all();
+                .statusCode(200).log().all()
+                .extract()
+                .response().as(PostDTO[].class);
+
+        PostDTO[] expectedListPostDTO = {
+                new PostDTO("test2", "test body2", "All Users", new AuthorDTO(USER_NAME), false),
+                new PostDTO("test", "test body", "All Users", new AuthorDTO(USER_NAME), false)
+        };
+
+        logger.info(expectedListPostDTO[0].toString());
+
+        Assert.assertEquals(responseBody.length, expectedListPostDTO.length);
+        SoftAssertions softAssertions = new SoftAssertions();
+
+        for (int i = 0; i < expectedListPostDTO.length; i++) {
+            softAssertions.assertThat(expectedListPostDTO[i])
+                    .isEqualToIgnoringGivenFields(responseBody[i], "_id", "createdDate", "author");
+            softAssertions.assertThat(expectedListPostDTO[i].getAuthor())
+                    .isEqualToIgnoringGivenFields(responseBody[i].getAuthor(), "avatar");
+        }
+
+
+        softAssertions.assertAll();
     }
+
+
+    @Test
+    public void getAllPostsByUserNegative(){
+        String responseBody =
+                given()
+                        .contentType(ContentType.JSON).log().all()
+                        .when()
+                        .get(POST_BY_USER,"notValidUser")
+                        .then()
+                        .statusCode(200).log().all()
+                        .extract().response().getBody().asString();
+
+        Assert.assertEquals("","\"Sorry, invalid user requested.undefined\"", responseBody);
+
+    }
+
+    @Test
+    public void getAllPostsByUserPath() {
+        Response responseBody =
+                given()
+                        .contentType(ContentType.JSON).log().all()
+                        .when()
+                        .get(POST_BY_USER, USER_NAME)
+                        .then()
+                        .statusCode(200).log().all()
+                        .extract().response();
+        List<String> titleList = responseBody.jsonPath().getList("title", String.class);
+        List<Map> authorList = responseBody.jsonPath().getList("author", Map.class);
+
+        SoftAssertions softAssertions = new SoftAssertions();
+        for (int i = 0; i < titleList.size(); i++) {
+            softAssertions.assertThat(titleList.get(i)).as("Item number " + i).contains("test");
+            softAssertions.assertThat(authorList.get(i).get("username")).as("Item number " + i).isEqualTo(USER_NAME);
+        }
+
+        softAssertions.assertAll();
+
+    }
+
+
+
+
 }
