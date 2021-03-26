e618d64d60e19054196837d2e2990110f57e89cf
Iryna_Lutskevych <ira98777@gmail.com>
2021-03-26 18:10:08 +0200

    Api test delete posts

--- lutskevych/src/test/java/apiTests/CreateNewPostByApiTest.java
+++ lutskevych/src/test/java/apiTests/CreateNewPostByApiTest.java
@@ -5,11 +5,18 @@ import api.AuthorDTO;
 import api.EndPoints;
 import api.PostDTO;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.assertj.core.api.SoftAssertions;
 import org.json.JSONObject;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.List;
+import java.util.Map;
+
+import static api.EndPoints.DELETE_POST;
+import static api.EndPoints.POST_BY_USER;
 import static io.restassured.RestAssured.given;
 
 public class CreateNewPostByApiTest {
@@ -17,12 +24,41 @@ public class CreateNewPostByApiTest {
     final String password = "123456qwerty";
     ApiHelper apiHelper = new ApiHelper();
 
+    @Before
+    public void postDeletion(){
+        String token = apiHelper.getToken(userName, password);
+
+        JSONObject requestParams = new JSONObject();
+        requestParams.put("token", token);
+
+        PostDTO[] responseBody = apiHelper.getAllPostsByUser(userName);
+
+
+        if (responseBody.length > 0){
+            for (int i = 0; i <responseBody.length; i++) {
+                String id = responseBody[i].get_id();
+                Response deleteBody =
+                        given()
+                                .contentType(ContentType.JSON).log().all()
+                                .body(requestParams.toMap()).log().all()
+                                .when()
+                                .delete(DELETE_POST, id)
+                                .then()
+                                .statusCode(200).log().all()
+                                .extract()
+                                .response();
+
+            }
+        }
+
+    }
+
     @Test
     public void createNewPosts() {
         String token = apiHelper.getToken(userName, password);
 
         JSONObject requestParams = new JSONObject();
-        requestParams.put("title", "New Post from API");
+        requestParams.put("title", "New Post from API222222");
         requestParams.put("body", "post body");
         requestParams.put("select1", "One Person");
         requestParams.put("token", token);
