423e6d2dd20fcc508c6f91a7729a25afc550b081
Kate Yurm <yurmanovych@gmail.com>
2021-03-30 16:33:08 +0100

    lesson 25/03

--- yurmanovych/src/main/java/api/Endpoints.java
+++ yurmanovych/src/main/java/api/Endpoints.java
@@ -1,10 +1,16 @@
 package api;
 
 public interface Endpoints {
-    String testAppBaseUrl = "http://qa-complex-app-for-testing.herokuapp.com";
-    String TEST_APP_POST_BY_USER = testAppBaseUrl + "/api/postsByAuthor/{1}";
 
+    // Privatbank API
     String pbBaseUrl = "https://api.privatbank.ua/p24api";
     String PB_GET_EXCH_RATES = pbBaseUrl + "/pubinfo?json&exchange&coursid=5";
     String PB_GET_BRANCHES = pbBaseUrl + "/pboffice?json&city={1}&address={2}";
+
+    // Testing app API
+    String testAppBaseUrl = "http://qa-complex-app-for-testing.herokuapp.com";
+    String TEST_APP_POST_BY_USER = testAppBaseUrl + "/api/postsByAuthor/{1}";
+    String LOGIN = testAppBaseUrl + "/api/login";
+    String CREATE_POST = testAppBaseUrl + "/api/create-post";
+
 }
