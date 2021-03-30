8a8c758a9c4d1c556992eee65a699dde93349160
Lena <lenazurich@gmail.com>
2021-03-30 20:26:05 +0200

    API Allure

--- chernenko/src/test/java/apiTest/CreateNewPostByApiTest.java
+++ chernenko/src/test/java/apiTest/CreateNewPostByApiTest.java
@@ -8,6 +8,7 @@ import io.restassured.http.ContentType;
 import org.assertj.core.api.SoftAssertions;
 import org.json.JSONObject;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
@@ -16,6 +17,11 @@ public class CreateNewPostByApiTest {
     final String userName = "cherchenko";
     final String passWord = "123456qwerty";
     ApiHelper apiHelper = new ApiHelper();
+
+    @Before
+    public void deleteAllPosts(){
+        apiHelper.deletePostsTillPresent(userName,passWord);
+    }
 
     @Test
     public void createNewPosts(){
