d3332fe2684aa5db39e479f7954c7dd5c22116fd
Yura Petrenko <bald2003@ukr.net>
2021-04-04 08:54:34 +0300

    apiTests allure

--- petrenko/src/main/java/api/ApiHelper.java
+++ petrenko/src/main/java/api/ApiHelper.java
@@ -1,8 +1,12 @@
 package api;
 
 
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.response.ResponseBody;
+import io.restassured.specification.RequestSpecification;
 import org.apache.log4j.Logger;
 import org.json.JSONObject;
 import org.junit.Assert;
@@ -12,6 +16,13 @@ import static io.restassured.RestAssured.given;
 
 
 public class ApiHelper {
+Logger logger = Logger.getLogger(getClass());
+
+RequestSpecification requestSpecification = new RequestSpecBuilder()
+        .setContentType(ContentType.JSON)
+        .log(LogDetail.ALL)
+        .addFilter(new AllureRestAssured())
+        .build();
 
 
 
@@ -22,7 +33,7 @@ public class ApiHelper {
         requestParams.put("password", password);
         ResponseBody responseBody =
                 given()
-                        .contentType(ContentType.JSON)
+                        .spec(requestSpecification)
                         .body(requestParams.toMap())
                         .when().post(EndPoints.LOGIN)
                         .then().statusCode(200)
@@ -34,7 +45,7 @@ public class ApiHelper {
     public PostDTO[] getAllPostsByUser(String userName) {
 
         PostDTO[] responseBody = given()
-                .contentType(ContentType.JSON).log().all()
+                .spec(requestSpecification)
 
                 .when()
 
@@ -61,8 +72,8 @@ public class ApiHelper {
         for (int i = 0; i < postDTO.length; i++) {
 
             given()
-                    .contentType(ContentType.JSON)
-                    .body(requestParams.toMap()).log().all()
+                    .spec(requestSpecification)
+                    .body(requestParams.toMap())
                     .when()
                     .delete(EndPoints.DELETE_POST, postDTO[i].get_id())
                     .then()
@@ -76,4 +87,37 @@ public class ApiHelper {
     }
 
 
+    public void deleteAllPostsTillPresent(String userName, String password) {
+        PostDTO[] listOfPostsByUser = getAllPostsByUser(userName);
+
+        for (int i = 0; i < listOfPostsByUser.length; i++) {
+
+            deletePostById(userName, password, listOfPostsByUser[i].get_id());
+                logger.info(String.format("Post with id %s and title %s was deleted"
+                        , listOfPostsByUser[i].get_id()
+                        , listOfPostsByUser[i].getTitle()));
+
+        }
+
+        Assert.assertEquals(0, getAllPostsByUser(userName).length );
+
+
+
+
+    }
+
+    private void deletePostById(String userName, String password, String id) {
+        String token = getToken(userName, password);
+        JSONObject bodyRequest = new JSONObject();
+        bodyRequest.put("token", token);
+        given()
+                .spec(requestSpecification)
+                .body(bodyRequest.toMap())
+        .when()
+                .delete(EndPoints.DELETE_POST, id)
+        .then()
+                .statusCode(200);
+
+
+    }
 }
