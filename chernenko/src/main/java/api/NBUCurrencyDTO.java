f8a97ff832ca6603917d8339981f2af049d0aa22
Lena <lenazurich@gmail.com>
2021-04-05 20:49:38 +0200

    Home Work Schema validation doesn't work.

---
+++ chernenko/src/main/java/api/NBUCurrencyDTO.java
@@ -0,0 +1,62 @@
+package api;
+
+public class NBUCurrencyDTO {
+    String ccy;
+    String base_ccy;
+    String buy;
+    String sale;
+
+
+    public NBUCurrencyDTO(String ccy, String base_ccy, String buy, String sale) {
+        this.ccy = ccy;
+        this.base_ccy = base_ccy;
+        this.buy = buy;
+        this.sale = sale;
+    }
+
+    public String getCcy() {
+        return ccy;
+    }
+
+    public void setCcy(String ccy) {
+        this.ccy = ccy;
+    }
+
+    public String getBase_ccy() {
+        return base_ccy;
+    }
+
+    public void setBase_ccy(String base_ccy) {
+        this.base_ccy = base_ccy;
+    }
+
+    public String getBuy() {
+        return buy;
+    }
+
+    public void setBuy(String buy) {
+        this.buy = buy;
+    }
+
+    public String getSale() {
+        return sale;
+    }
+
+    public void setSale(String sale) {
+        this.sale = sale;
+    }
+
+    public NBUCurrencyDTO(){
+
+    }
+
+    @Override
+    public String toString() {
+        return "NBUCurrencyDTO{" +
+                "ccy='" + ccy + '\'' +
+                ", base_ccy='" + base_ccy + '\'' +
+                ", buy='" + buy + '\'' +
+                ", sale='" + sale + '\'' +
+                '}';
+    }
+}
---
+++ chernenko/src/test/java/apiTest/NBUCurrencyResponseTest.java
@@ -0,0 +1,70 @@
+package apiTest;
+
+import api.NBUCurrencyDTO;
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.http.ContentType;
+import org.apache.log4j.Logger;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+
+public class NBUCurrencyResponseTest {
+    Logger logger = Logger.getLogger(getClass());
+
+    @Test
+    public void currencyResponseNBUSchemaValidationTest(){
+        given()
+                .contentType(ContentType.JSON).log().all()
+                .filter(new AllureRestAssured())
+         .when()
+                .get("https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5")
+         .then()
+                .statusCode(200).log().all()
+                .assertThat().body(matchesJsonSchemaInClasspath("responsenbu.json"));
+    }
+
+    @Test
+    public void currencyResponseNBUSchemaTest(){
+       NBUCurrencyDTO[] bodyResponse =
+        given()
+                .contentType(ContentType.JSON).log().all()
+                .filter(new AllureRestAssured())
+        .when()
+                .get("https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5")
+        .then()
+                .statusCode(200).log().all()
+                .extract()
+                .response().as(NBUCurrencyDTO[].class);
+                //.assertThat().body(matchesJsonSchemaInClasspath("responseNBU.json"));
+        NBUCurrencyDTO[] expectedListNBUCurrencyDTO = {
+         new NBUCurrencyDTO("USD","UAH","27.70000","28.10000"),
+         new NBUCurrencyDTO("EUR","UAH","32.50000","33.12000"),
+         new NBUCurrencyDTO("RUR", "UAH","0.35700","0.38700"),
+         new NBUCurrencyDTO("BTC", "USD","54722.4915","60482.7537")
+        };
+
+
+        logger.info(expectedListNBUCurrencyDTO);
+        Assert.assertEquals(bodyResponse.length,expectedListNBUCurrencyDTO.length);
+        SoftAssertions softAssertions = new SoftAssertions();
+
+        for (int i = 0; i <bodyResponse.length ; i++) {
+            softAssertions.assertThat(expectedListNBUCurrencyDTO[i]).isEqualToIgnoringGivenFields(bodyResponse[i], "buy","sale");
+            logger.info("Exchange   " + bodyResponse[i].getCcy()+ "  to  " + bodyResponse[i].getBase_ccy() + "  buy   "
+                    + bodyResponse[i].getBuy()+ "  sale  " + bodyResponse[i].getSale() );
+
+
+        }
+
+
+
+        softAssertions.assertAll();
+    }
+}
