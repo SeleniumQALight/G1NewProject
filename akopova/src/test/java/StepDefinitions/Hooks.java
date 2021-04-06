df45f9f962cbc98f73887f0a0b3c827638698b62
Inga <inga_ai@yahoo.com>
2021-04-06 17:47:52 +0300

    DriverHelper  +2 files refactored

--- akopova/src/test/java/StepDefinitions/Hooks.java
+++ akopova/src/test/java/StepDefinitions/Hooks.java
@@ -3,22 +3,22 @@ package StepDefinitions;
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
-import libs.DriverManager;
+import libs.DriverHelper;
 import org.apache.log4j.Logger;
 
 public class Hooks {
 
-    private DriverManager driverManager = new DriverManager();
+    private DriverHelper driverHelper = new DriverHelper();
     Logger logger = Logger.getLogger(getClass());
 
     @Before
     public void setUp(Scenario scenario){
-        driverManager.createDriver();
+        driverHelper.createDriver();
     }
 
     @After
     public void tearDown(){
-        driverManager.closeDriver();
+        driverHelper.closeDriver();
     }
 
 
