4efb31741b81a3325aa1342b14d3802a823db3a1
yuliia.dykovytska <yuliya.dikovitskaya@gmail.com>
2021-04-06 18:58:50 +0300

    Tests with Cucumber (fix)

--- dikovitska/src/test/java/StepDefinitions/Hooks.java
+++ dikovitska/src/test/java/StepDefinitions/Hooks.java
@@ -3,11 +3,11 @@ package StepDefinitions;
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
-import libs.DriverManager;
+import libs.DriverHelper;
 import org.apache.log4j.Logger;
 
 public class Hooks {
-    private DriverManager driverManager = new DriverManager();
+    private DriverHelper driverManager = new DriverHelper();
     Logger logger = Logger.getLogger(getClass());
 
     @Before
