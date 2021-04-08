75e4805db8163a0a8a1ba4c8ff911d1f4c02ae7b
Lenovo <yakovenkors@gmail.com>
2021-04-08 21:32:15 +0300

    Cucumber Tests + API

---
+++ iakovenko/src/test/java/StepDefinitions/API_StepDefinitions.java
@@ -0,0 +1,24 @@
+package StepDefinitions;
+
+import api.ApiHelper;
+import cucumber.api.java.en.Given;
+import libs.TestData;
+
+public class API_StepDefinitions {
+    private  ApiHelper apiHelper = new ApiHelper();
+    final String DEFAULT = "default";
+    @Given("^Create (\\d+) new posts via API for '(.*)' user and '(.*)' password$")
+    public void createNewPostsViaAPIForDefaultUserAndDefaultPassword(int numberOfPosts, String userName, String passWord) {
+        if (DEFAULT.equals(userName)){
+            userName = TestData.VALID_LOGIN;
+        }
+        if ((DEFAULT.equals(passWord))){
+            passWord = TestData.VALID_PASSWORD;
+        }
+        for (int i = 0; i <numberOfPosts ; i++) {
+            apiHelper.createPost("Post from API" + i, userName, passWord);
+
+
+        }
+    }
+}
