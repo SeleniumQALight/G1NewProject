4bfd3b3d15deba7577c6228ebb5478cd32961a3b
Yura Petrenko <bald2003@ukr.net>
2021-04-09 08:20:23 +0300

    refactor cucumber

---
+++ petrenko/src/test/java/StepDefinitions/Hooks.java
@@ -0,0 +1,27 @@
+package StepDefinitions;
+
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import libs.DriverHelper;
+import org.apache.log4j.Logger;
+
+public class Hooks {
+
+    private DriverHelper driverHelper = new DriverHelper();
+    Logger logger = Logger.getLogger(getClass());
+    @Before
+    public void setUp(Scenario scenario){
+        driverHelper.closeDriver();
+
+
+
+    }
+
+
+    @After
+    public void tearDown(){
+        driverHelper.closeDriver();
+    }
+
+}
---
+++ petrenko/src/test/java/StepDefinitions/LoginPage_StepDefinitions.java
@@ -0,0 +1,18 @@
+package StepDefinitions;
+
+import cucumber.api.java.en.Given;
+import libs.DriverHelper;
+import pages.LoginPage;
+
+public class LoginPage_StepDefinitions {
+
+    private LoginPage loginPage = new LoginPage(DriverHelper.getWebDriver());
+
+    @Given("^User opens 'Login' page$")
+    public void OpenLoginPag(){
+    loginPage.openLoinPage();
+
+
+    }
+
+}
