fc1e73dfb0bb61342b81663efc70d6cda282e5d9
Taras Radulenko <Taras_Radulenko@epam.com>
2021-04-14 10:05:43 +0300

    cucumber allure screenshots

--- overAll/src/test/java/StepDefinitions/Hooks.java
+++ overAll/src/test/java/StepDefinitions/Hooks.java
@@ -1,11 +1,22 @@
 package StepDefinitions;
 
+import java.io.ByteArrayInputStream;
+
 import org.apache.log4j.Logger;
+import org.junit.Rule;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriverException;
 
 import api.ApiHelper;
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
+import io.qameta.allure.Allure;
+import io.qameta.allure.Attachment;
+import io.qameta.allure.Step;
 import libs.DriverHelper;
 import libs.TestData;
 
@@ -15,21 +26,40 @@ public class Hooks {
     private ApiHelper apiHelper = new ApiHelper();
 
     @Before(order = 0)
-    public void setUp(Scenario scenario){
+    public void setUp(Scenario scenario) {
         logger.info(scenario.getName() + " was started");
+        logger.info(scenario.getId() + " scenarioID");
         driverHelper.createDriver();
     }
 
-    @After(order = 100)
-    public void tearDown(Scenario scenario){
+    @After(order = 0)
+    public void tearDown(Scenario scenario) {
         driverHelper.closeDriver();
+        logger.info(scenario.getId() + " scenarioID");
         logger.info(scenario.getName() + " was ended with status " + scenario.getStatus());
     }
 
     @Before(value = "@BeforeDeletingAllPostsForDefaultUser", order = 100)
     @After(value = "@AfterDeletingAllPostsForDefaultUser", order = 50)
-    public void deletingAllPostsForDefaultUser(){
+    public void deletingAllPostsForDefaultUser() {
         apiHelper.deletePostsTillPresent(TestData.VALID_LOGIN, TestData.VALID_PASSWORD);
+    }
+
+    @After(order = 100)
+    public void embedScreenshot(Scenario scenario) {
+        if (scenario.isFailed()) {
+            try {
+//                Allure.attachment("nameTest1",
+//                        new ByteArrayInputStream(((TakesScreenshot) DriverHelper.getWebDriver()).getScreenshotAs(OutputType.BYTES)));
+                scenario.write("Current Page URL is " + DriverHelper.getWebDriver().getCurrentUrl());
+                byte[] screenshot = ((TakesScreenshot) DriverHelper.getWebDriver()).getScreenshotAs(OutputType.BYTES);
+                scenario.embed(screenshot, "image/png");  // Stick it in the report
+            } catch (WebDriverException somePlatformsDontSupportScreenshots) {
+                System.out.println(somePlatformsDontSupportScreenshots.getMessage());
+            } catch (ClassCastException cce) {
+                cce.printStackTrace();
+            }
+        }
     }
 
 }
--- overAll/src/test/resources/postsTests/post.feature
+++ overAll/src/test/resources/postsTests/post.feature
@@ -8,7 +8,7 @@ Feature: Post feature
   @BeforeDeletingAllPostsForDefaultUser
   @AfterDeletingAllPostsForDefaultUser
   Scenario: R002 Check number of posts
-    And Create 25 new posts via API for 'default' user and 'default' password
+    And Create 2 new posts via API for 'default' user and 'default' password
     When User click on 'Profile' button
     Then User is redirect to 'Profile' page
-    And User sees 25 posts in 'Posts list' on 'Profile' page
+    And User sees 2 posts in 'Posts list' on 'Profile' page
